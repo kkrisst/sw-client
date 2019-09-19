97b0989cb520775355b1f60b8176cc9a61d6f845
Kristóf Kövér <kristof.kover@lightware.com>
2019-09-19 12:30:26 +0200

    Added fetchRoot call to ResourceOverview, displaying the first 10 hits and a Load more div

--- src/components/resource-overview/resource-overview.jsx
+++ src/components/resource-overview/resource-overview.jsx
@@ -1,11 +1,42 @@
 import React, { useEffect } from 'react';
 
+import { fetchRoot } from '../../actions';
+import { useDispatch, useSelector } from 'react-redux';
+
 import './resource-overview.scss';
 
 const ResourceOverview = ({ rootId }) => {
+  const dispatch = useDispatch();
+
+  const resources = useSelector(state => state.resources.payload[rootId]);
+
+  useEffect(() => {
+		dispatch(fetchRoot(rootId));
+  }, [dispatch]);
+  
+  const resourceTtems = (resources || []);
+
   return (
     <div>
-      Resource Overview {rootId}
+      <div>
+        {resources && (
+          resourceTtems.results.map( (item, index) => {
+            return (
+              <div key={index}>{item.name}</div>
+            )
+          })
+        )}
+      </div>
+      {
+        resources && resources.next
+        ? (
+          <div>
+          Load more
+          </div>
+        )
+        : ''
+      }
+      
     </div>
   );
 };
